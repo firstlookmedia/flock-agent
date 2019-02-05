91d1545864af456dd8568e136549bdf8ee0a7ead
Micah Lee <micah@micahflee.com>
2019-02-04 18:03:06 -0800

    Start making flock-agent a python program

---
+++ setup.py
@@ -0,0 +1,28 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="flock-agent",
+    version="0.1",
+    author="Micah Lee",
+    author_email="micah.lee@firstlook.org",
+    description="Join a privacy-preserving, centrally-managed Flock fleet",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/firstlookmedia/flock-agent",
+    packages=['flock_agent'],
+    package_data={'flock_agent': ['config/*']},
+    classifiers=(
+        "Development Status :: 4 - Beta",
+        "Programming Language :: Python",
+        "Intended Audience :: End Users/Desktop",
+        "Operating System :: OS Independent"
+    ),
+    entry_points={
+        'console_scripts': [
+            'flock_agent = flock_agent:main',
+        ],
+    },
+)
