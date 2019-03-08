5b95343231920ee48cb1d43d1d1a2cf1df15b96f
Micah Lee <micah@micahflee.com>
2019-03-07 17:40:23 -0800

    Change the heartbeat interval from 1 minute to 10 minutes

--- flock_agent/twigs.py
+++ flock_agent/twigs.py
@@ -49,8 +49,8 @@ twigs = {
     },
     "heartbeat": {
         "name": "Heartbeat",
-        "description": "A heartbeat counter that reports general performance (CPU, memory) and version.",
+        "description": "A heartbeat counter that reports general performance (CPU, memory) and version",
         "query": "select i.*, p.resident_size, p.user_time, p.system_time, time.minutes as counter from osquery_info i, processes p, time where p.pid = i.pid;",
-        "interval": 60
+        "interval": 600
     }
 }
