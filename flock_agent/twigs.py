c9d6d23ad320b531a4a2d2e2ebe33494b72a3bf6
Micah Lee <micah@micahflee.com>
2019-03-08 15:35:35 -0800

    Comment out heartbeat twig

--- flock_agent/twigs.py
+++ flock_agent/twigs.py
@@ -47,10 +47,10 @@ twigs = {
         "query": "SELECT path, label, keep_alive, username, program_arguments FROM launchd WHERE label NOT LIKE 'com.apple.%' AND disabled != 1;",
         "interval": 600
     },
-    "heartbeat": {
-        "name": "Heartbeat",
-        "description": "A heartbeat counter that reports general performance (CPU, memory) and version",
-        "query": "select i.*, p.resident_size, p.user_time, p.system_time, time.minutes as counter from osquery_info i, processes p, time where p.pid = i.pid;",
-        "interval": 600
-    }
+    #"heartbeat": {
+    #    "name": "Heartbeat",
+    #    "description": "A heartbeat counter that reports general performance (CPU, memory) and version",
+    #    "query": "select i.*, p.resident_size, p.user_time, p.system_time, time.minutes as counter from osquery_info i, processes p, time where p.pid = i.pid;",
+    #    "interval": 600
+    #},
 }
