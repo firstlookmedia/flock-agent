4e2c1ff80a548cc1f7af3d55a1e897b295ffc65e
Micah Lee <micah@micahflee.com>
2019-03-07 17:18:18 -0800

    Automatically submit osquery logs, in a background thread in the systray

--- flock_agent/gui/systray.py
+++ flock_agent/gui/systray.py
@@ -8,3 +8,37 @@ class SysTray(QtWidgets.QSystemTrayIcon):
 
         # Show the systray icon
         self.show()
+
+        # Submit osquery logs to the server each minute
+        self.currently_submitting = False
+        self.submit_timer = QtCore.QTimer()
+        self.submit_timer.timeout.connect(self.run_submit)
+        self.submit_timer.start(60000) # 1 minute
+
+    def run_submit(self):
+        if self.currently_submitting:
+            return
+        self.currently_submitting = True
+
+        self.t = SubmitThread(self.c)
+        self.t.submit_finished.connect(self.submit_finished)
+        self.t.start()
+
+    def submit_finished(self):
+        self.currently_submitting = False
+
+
+class SubmitThread(QtCore.QThread):
+    """
+    Submit osquery records to the Flock server
+    """
+    submit_finished = QtCore.pyqtSignal()
+
+    def __init__(self, common):
+        super(SubmitThread, self).__init__()
+        self.c = common
+
+    def run(self):
+        self.c.log('SubmitThread', 'run')
+        self.c.osquery.submit_logs()
+        self.submit_finished.emit()
