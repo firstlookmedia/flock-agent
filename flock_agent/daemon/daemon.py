0463c2cd9a65b5f2b6bf75502636a9a640cf93f2
Micah Lee <micah@micahflee.com>
2019-10-02 21:40:56 -0400

    Start aiohttp web server on a unix socket, and give the socket proper permissions

--- flock_agent/daemon/daemon.py
+++ flock_agent/daemon/daemon.py
@@ -1,5 +1,9 @@
 # -*- coding: utf-8 -*-
+import os
+import grp
 import asyncio
+from aiohttp import web
+
 from .global_settings import GlobalSettings
 from .osquery import Osquery
 
@@ -14,15 +18,56 @@ class Daemon:
 
         self.global_settings = GlobalSettings(common)
 
+        # Prepare the unix socket path
+        self.unix_socket_path = '/var/lib/flock-agent/socket'
+        os.makedirs('/var/lib/flock-agent', exist_ok=True)
+        if os.path.exists(self.unix_socket_path):
+            os.remove(self.unix_socket_path)
+
+        # The socket uid will be 0 (root), and the group will be the administrator group. In debian-like
+        # distros it's the "sudo" group, and in fedora-like distros it's the "wheel" group.
+        # Other distros? PRs are welcome :).
+        if os.path.isfile('/usr/bin/apt'):
+            groupinfo = grp.getgrnam('sudo')
+            self.gid = groupinfo.gr_gid
+        elif os.path.isfile('/usr/bin/dnf') or os.path.isfile('/usr/bin/yum'):
+            groupinfo = grp.getgrnam('wheel')
+            self.gid = groupinfo.gr_gid
+        else:
+            # Unknown, so make the group root
+            self.gid = 0
+
     async def start(self):
+        await asyncio.gather(
+            self.submit_loop(),
+            self.http_server()
+        )
+
+    async def submit_loop(self):
         while True:
             # Submit osquery logs
-            self.c.log("Daemon", "start", "Submitting osquery logs")
+            self.c.log("Daemon", "submit_loop", "Submitting osquery logs")
             try:
                 self.osquery.submit_logs()
             except Exception as e:
                 exception_type = type(e).__name__
-                self.c.log("Daemon", "start", "Exception submitting logs: {}".format(exception_type))
+                self.c.log("Daemon", "submit_loop", "Exception submitting logs: {}".format(exception_type))
 
             # Wait a minute
             await asyncio.sleep(60)
+
+    async def http_server(self):
+        self.c.log("Daemon", "http_server", "Starting http server")
+
+        async def hello(request):
+            return web.Response(text="Hello, world")
+
+        app = web.Application()
+        app.router.add_get('/', hello)
+
+        loop = asyncio.get_event_loop()
+        await loop.create_unix_server(app.make_handler(), self.unix_socket_path)
+
+        # Change permissions of unix socket
+        os.chmod(self.unix_socket_path, 0o660)
+        os.chown(self.unix_socket_path, 0, self.gid)
